af3a2ebcf948ebb90f490fb6b2b6897ed38dec6e
hrypasato <hrypasato@gmail.com>
2023-09-28 21:40:10 -0500

    seccion 27

---
+++ flutter-authenticated/lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+  return AuthNotifier(authRepository: AuthRepositoryImpl());
+});
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthRepository authRepository;
+
+  AuthNotifier({required this.authRepository}) : super(AuthState());
+
+  Future<void> loginUser(String email, String password) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout(errorMessage: 'Credenciales no validas');
+    } catch (e) {
+      logout(errorMessage: 'Error interno');
+    }
+  }
+
+  void registerUser(String email, String password) async {}
+
+  void checkAuthStatus() async {}
+
+  void _setLoggedUser(User user) {
+    state = state.copyWith(
+      user: user,
+      errorMessage: '',
+    );
+  }
+
+  Future<void> logout({String? errorMessage}) async {
+    state = state.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: errorMessage,
+    );
+  }
+}
+
+enum AuthStatus { checking, authenticated, notAuthenticated }
+
+class AuthState {
+  final AuthStatus authStatus;
+  final User? user;
+  final String errorMessage;
+
+  AuthState({
+    this.authStatus = AuthStatus.checking,
+    this.user,
+    this.errorMessage = '',
+  });
+
+  AuthState copyWith({
+    AuthStatus? authStatus,
+    User? user,
+    String? errorMessage,
+  }) =>
+      AuthState(
+        authStatus: authStatus ?? this.authStatus,
+        user: user ?? this.user,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
+}
