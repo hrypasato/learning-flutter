6b77d14f1a96e12a567509a3e6c2290216bd5240
hrypasato <hrypasato@gmail.com>
2023-11-27 12:02:11 -0500

    feat: add images assets

--- complete_advanced_flutter/lib/app/app.dart
+++ complete_advanced_flutter/lib/app/app.dart
@@ -20,6 +20,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: getApplicationTheme(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
